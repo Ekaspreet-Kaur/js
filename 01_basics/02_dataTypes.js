925636dc2d24397ab4d6c20997b0e1a2495d9c9e
Ekaspreet-Kaur <ekaspreetkaur1706@gmail.com>
2025-07-16 07:14:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3)  //we are using node.js and not the browser
+
+console.log( 3
+    +
+    3 )  // code readibility should be high
+console.log("Ekaspreet")
+
+let name = "Ekaspreet" //string
+let age  = 20 // number
+let isLoggedin = false //boolean
+        //Primitive data types
+// number =>  2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value , khaali hai
+// undefined => value define hi nhi ki hai variable ki
+// symbol => finds the uniqueness
+     
+//object
+
+console.log(typeof "Ekaspreet") //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
